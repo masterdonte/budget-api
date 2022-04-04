ed460792abd3e29dacf65f28f4ad259b365f1110
Jonathas <master.donte@hotmail.com>
2022-04-04 16:23:16 -0300

    Changing Password

--- src/main/java/com/donte/budget/api/security/util/GeradorSenha.java
+++ src/main/java/com/donte/budget/api/security/util/GeradorSenha.java
@@ -6,10 +6,10 @@ public class GeradorSenha {
 
 	public static void main(String[] args) {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		System.out.println(encoder.encode("em@p2016"));
+		System.out.println(encoder.encode("Abc123"));
 		
-		System.out.println(encoder.matches("em@p2016", "$2a$10$rKvZjZTt4n9jMhfbkKwB4.dUjD9z84ZW0U92P8BIRAlp7a.VQdoFW"));
-		System.out.println(encoder.matches("em@p2016", "$2a$10$CXt10CVBgQOvU8JIKBoxqeMBoJp8XvRBrhJRxc36MD25g7EOWQXMa"));
+		System.out.println(encoder.matches("Abc123", "$2a$10$rKvZjZTt4n9jMhfbkKwB4.dUjD9z84ZW0U92P8BIRAlp7a.VQdoFW"));
+		System.out.println(encoder.matches("Abc123", "$2a$10$CXt10CVBgQOvU8JIKBoxqeMBoJp8XvRBrhJRxc36MD25g7EOWQXMa"));
 		
 	//	apasswordEncoder.matches(lSenhaAntiga, aUsuario.getPassword())
 	}
